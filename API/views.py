b8680d64a1ee3f11e700d3e5ca726cd8523f3ed4
Ib Lundgren <ib@ib-laptop.(none)>
2011-11-29 14:04:08 +0100

    apparently appengine no longer supports djangoforms

--- API/views.py
+++ API/views.py
@@ -1,21 +1,23 @@
 from django.views.generic.simple import direct_to_template
-from forms import *
 from models import *
 from django.utils import simplejson
 from django.http import HttpResponseRedirect
 
 def test(request):
-	go = GameObjectForm()
-	return direct_to_template(request, 'api/form.html', {"form":go}) 
+	return direct_to_template(request, 'api/form.html', {}) 
 
 def objects(request):
 	if request.method == 'GET':
             items = simplejson.dumps([o.to_dict() for o in GameObject.all()], indent=3)
 	    return direct_to_template(request, 'api/list.html', { "items":items})
 	elif request.method == 'POST':
-	    item = GameObjectForm(data=request.POST)
-	    if item.is_valid():
-		item.save()
+	    title = request.POST.get("title")
+	    description = request.POST.get("description")
+	    if title and description:
+		go = GameObject()
+		go.title = title
+		go.description = description
+		go.put()
 		return HttpResponseRedirect('/api/objects')
 	    else:
 		items = simplejson.dumps(dict("error", "invalid input"))
